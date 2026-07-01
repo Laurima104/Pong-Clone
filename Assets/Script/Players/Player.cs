a517bff7883c43db7be7d47c97c150d814d2cda2
Laurima104 <132705826+Laurima104@users.noreply.github.com>
2026-07-01 10:04:31 -0300

    feat: adding the player2 controls

---
+++ Assets/Script/Players/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform position;
+    [SerializeField] private float velocity;
+    [SerializeField] private bool isPlayer;
+    [SerializeField] private int playerId;
+    void Update()
+    {
+        if (isPlayer)
+        {
+            if (playerId == 1 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+            {
+                Movement();
+            } else if(playerId == 2 && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)))
+            {
+                Movement();
+            }
+        }
+
+
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.up * velocity * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += -transform.up * velocity * Time.deltaTime;
+        }
+    }
+}
